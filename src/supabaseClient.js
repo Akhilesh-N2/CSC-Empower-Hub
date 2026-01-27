c021bb6e87d2b0bb6a9116741f2b5c61d9d1642e
Akhilesh N <akhileshthrissur@gmail.com>
2026-01-28 00:16:26 +0530

    Moved API keys to environment variables

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// GO TO: Supabase Dashboard -> Settings (Gear Icon) -> API
-// Copy "Project URL" and "anon" (public) Key
-const supabaseUrl = 'https://tpvrauyvtnogsygkkwlz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwdnJhdXl2dG5vZ3N5Z2trd2x6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1MTQ1NDUsImV4cCI6MjA4NTA5MDU0NX0.aHtWuPL2R82o4DJnAvzw9hxBhTyVEnxkO-njft2cxwg'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
